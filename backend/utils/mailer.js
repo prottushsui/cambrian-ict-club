4c514f694015d78ddc7f51ae217aace9cb6833f2
prottushsui <prottush2011@gmail.com>
2025-09-05 00:45:09 +0600

    Update mailer.js

--- backend/utils/mailer.js
+++ backend/utils/mailer.js
@@ -1,1 +1,31 @@
+const nodemailer = require('nodemailer');
 
+const sendPasswordResetEmail = async (email, token) => {
+  const transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    secure: false,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS,
+    },
+  });
+
+  const resetUrl = `http://localhost:3000/reset-password.html?token=${token}`;
+
+  const mailOptions = {
+    to: email,
+    from: 'no-reply@ictclub.cambrian.edu',
+    subject: 'Password Reset Request',
+    html: `
+      <h2>Password Reset</h2>
+      <p>You requested a password reset.</p>
+      <p>Click <a href="${resetUrl}">here</a> to reset your password.</p>
+      <p>If you didn't request this, ignore this email.</p>
+    `,
+  };
+
+  await transporter.sendMail(mailOptions);
+};
+
+module.exports = { sendPasswordResetEmail };
