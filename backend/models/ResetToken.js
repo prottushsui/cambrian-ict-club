94795a3de6c36a6f8e188a9bad85fa7115e43144
prottushsui <prottush2011@gmail.com>
2025-09-05 00:43:42 +0600

    Update ResetToken.js

--- backend/models/ResetToken.js
+++ backend/models/ResetToken.js
@@ -1,1 +1,9 @@
+const mongoose = require('mongoose');
 
+const resetTokenSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  token: { type: String, required: true },
+  expiresAt: { type: Date, default: () => Date.now() + 3600000 },
+});
+
+module.exports = mongoose.model('ResetToken', resetTokenSchema);
