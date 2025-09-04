7f1b9f7c78722b153b786b8d55763fea60505c83
prottushsui <prottush2011@gmail.com>
2025-09-05 00:47:47 +0600

    Update voteRoutes.js

--- backend/routes/voteRoutes.js
+++ backend/routes/voteRoutes.js
@@ -1,1 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const auth = require('../middleware/auth');
+const voteController = require('../controllers/voteController');
 
+router.post('/', auth, voteController.submitVote);
+
+module.exports = router;
