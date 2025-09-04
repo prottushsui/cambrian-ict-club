7102ec7c9f918b424bb9bc5c0d7c1d2f1085013d
prottushsui <prottush2011@gmail.com>
2025-09-05 01:27:19 +0600

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,9 +2,16 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 
+// Login
 router.post('/login', authController.login);
+
+// Registration
 router.post('/register', authController.register);
+
+// Forgot Password
 router.post('/forgot-password', authController.forgotPassword);
+
+// Reset Password
 router.post('/reset-password', authController.resetPassword);
 
 module.exports = router;
